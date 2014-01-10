a7cf7ca1429ed6847439c5e97fc9376360b462c7
Sue Pan <pansu@mail.nih.gov>
2014-01-10 09:14:11 -0500

    remove obsolete directory

--- src/Biomarker-Rest-Project/DrawROC.R
+++
@@ -1,7 +0,0 @@
-DrawROC <- function(Delta) {
-	jpeg("rplot.jpg")
-	x<-seq(from=0,to=1,by=0.0001)
-	y<-1-pnorm(qnorm(x,0,1)-Delta,0,1,1)
-	plot(y~x, main=c("ROC Curve, Sensitivity vs. Specificity",paste(expression(Delta),"=",Delta)),xlab="Specificity",ylab="Sensitivity",type='l', lwd=1,xlim=rev(range(x)),font.lab=2,font=2,cex.axis=1.15,cex.lab=1.15)
-	dev.off()
-}
