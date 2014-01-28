69cf2f68b5e420339ff3777d80dd0bbc013bf8b7
Shaun Einolf <einolfs@mail.nih.gov>
2014-01-28 13:33:04 -0500

    Revert "removed alert in js file"
    This reverts commit 1ddce4b61aae236da06570e9aa6fd7c10ae45435.

--- src/apc/static/apc.js
+++ src/apc/static/apc.js
@@ -171,6 +171,7 @@ $(document).ready(function() {
 
         $('#countPopulation').change(function(){
                 if ( window.FileReader ) {
+                        alert("read");
                         var file = this.files[0];
                         var reader = new FileReader();
                         reader.onload = function(theFile) {
