39de6af719c173f8022a02a656e83370714994ee
Sue Pan <pansu@mail.nih.gov>
2014-02-26 10:35:25 -0500

    updated to reflect the latest code from Christine (legend outside of the graph)

--- src/bc/r-code/DrawSensSpecLR.R
+++ src/bc/r-code/DrawSensSpecLR.R
@@ -1,6 +1,6 @@
 DrawSensSpecLR <- function(uniquekey, sensref, specref,sensitivity,specificity, names) {
-  file<- paste("./img/",uniquekey,"SensSpecLR.png")
-  graphfile<-gsub("\\s","", file)
+  file<-paste("./img/", uniquekey, "SensSpecLR.jpg")
+  graphfile<-gsub("\\s", "", file)
   png(graphfile)
   Specificity <- seq(from=0,to=1,by=0.00001)
   LRMinus <- specref/(1-sensref)
@@ -14,6 +14,7 @@ DrawSensSpecLR <- function(uniquekey, sensref, specref,sensitivity,specificity, 
     Sensitivity1 <- 1 - Specificity/LRMinus
     Sensitivity2 <- LRPlus*(1 - Specificity)
     if(i==1) {
+      par(mar=c(5.1, 4.1, 4.1, 10))
       plot(Sensitivity1~Specificity,type="l",main=c("Sensitivity vs. Specificity","with Likelihood Ratio contours"),xlab="Specificity",ylab="Sensitivity",ylim=c(0,1),xlim=rev(range(Specificity)),lty=LTY[i],col=LTY[i],font.lab=2,font=2,cex.axis=1.15,cex.lab=1.15)
     }
     else {
@@ -23,6 +24,6 @@ DrawSensSpecLR <- function(uniquekey, sensref, specref,sensitivity,specificity, 
     }
   }
   
-  legend(lty=LTY,col=LTY,legend=c(paste("LR-",round(LRMinus,digits=1)),paste("LR+",round(LRPlus,digits=1))),"bottomright",cex=1.15,text.font=2,bty="o",title="Likelihood Ratio (LR)") 
+  legend(inset=c(-0.5,0),lty=LTY,col=LTY,legend=c(paste("LR-",round(LRMinus,digits=1)),paste("LR+",round(LRPlus,digits=1))),"bottomright",cex=1.15,text.font=2,bty="o",xpd = TRUE,pch=c(1,3),title="Likelihood Ratio (LR)") 
   dev.off()
 }
