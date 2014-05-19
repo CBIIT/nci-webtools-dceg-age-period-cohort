18cb818c208b7c2839cc5fff383e56b521cec6ef
bcoffe <coffeybrent@gmail.com>
2014-05-19 18:50:47 -0400

    Forgot that I needed to combine sensitity spec with all tabs..so fixed it, easy enough

--- src/meanstorisk/writeToExcel.R
+++ src/meanstorisk/writeToExcel.R
@@ -1,14 +1,14 @@
 library('xlsx');
 
-#excelDirectory <- "/home/brent/development/nci-analysis-tools-web-presence/src/meanstorisk/xlsx/";
+excelDirectory <- "/home/brent/development/nci-analysis-tools-web-presence/src/meanstorisk/xlsx/";
 
-excelDirectory <- "./xlsx/";
+#excelDirectory <- "./xlsx/";
 
 writeResultsToExcel <- function (risk, graphName) {
   outwb <- createWorkbook();
   
   deltaSheet            <- createSheet(outwb, sheetName = "Delta");
-  sgsSheet              <- createSheet(outwb, sheetName = "Sensitivity Given Specificity");
+
   ppvSheet              <- createSheet(outwb, sheetName = "PPV");
   cNpvSheet             <- createSheet(outwb, sheetName = "cNPV");
   ppvCnpvSheet          <- createSheet(outwb, sheetName = "PPV-cNPV");
@@ -18,14 +18,27 @@ writeResultsToExcel <- function (risk, graphName) {
   dominatedSheet        <- createSheet(outwb, sheetName = "Dominated by Specificity for a Rare Disease");
   
   addDataFrame(x = as.data.frame.matrix(risk$Delta), sheet = deltaSheet, row.names=TRUE, col.name=TRUE);
-  addDataFrame(x = as.data.frame.matrix(risk$`Sensitivity Given Specificity`), sheet = sgsSheet, row.names=TRUE, col.name=TRUE);
-  addDataFrame(x = as.data.frame.matrix(risk$PPV), sheet = ppvSheet, row.names=TRUE, col.name=TRUE);
-  addDataFrame(x = as.data.frame.matrix(risk$cNPV), sheet = cNpvSheet, row.names=TRUE, col.name=TRUE);
-  addDataFrame(x = as.data.frame.matrix(risk$`PPV-cNPV`), sheet = ppvCnpvSheet, row.names=TRUE, col.name=TRUE);
-  addDataFrame(x = as.data.frame.matrix(risk$`Program-Based`), sheet = programBasedSheet, row.names=TRUE, col.name=TRUE);
-  addDataFrame(x = as.data.frame.matrix(risk$`PPV-Based`), sheet = ppvBasedSheet, row.names=TRUE, col.name=TRUE);
-  addDataFrame(x = as.data.frame.matrix(risk$`Sensitivity-Based`), sheet = sensitivityBasedSheet, row.names=TRUE, col.name=TRUE);
-  addDataFrame(x = as.data.frame.matrix(risk$`Dominated by Specificity for a Rare Disease`), sheet = dominatedSheet, row.names=TRUE, col.name=TRUE);
+  
+  addDataFrame(x = as.data.frame.matrix(risk$`Sensitivity Given Specificity`), sheet = ppvSheet, row.names=TRUE, col.name=TRUE);
+  addDataFrame(x = as.data.frame.matrix(risk$PPV), sheet = ppvSheet, row.names=FALSE, col.name=TRUE,startColumn=6);
+  
+  addDataFrame(x = as.data.frame.matrix(risk$`Sensitivity Given Specificity`), sheet = cNpvSheet, row.names=TRUE, col.name=TRUE);
+  addDataFrame(x = as.data.frame.matrix(risk$cNPV), sheet = cNpvSheet, row.names=FALSE, col.name=TRUE, startColumn=6);
+  
+  addDataFrame(x = as.data.frame.matrix(risk$`Sensitivity Given Specificity`), sheet = ppvCnpvSheet, row.names=TRUE, col.name=TRUE);
+  addDataFrame(x = as.data.frame.matrix(risk$`PPV-cNPV`), sheet = ppvCnpvSheet, row.names=FALSE, col.name=TRUE, startColumn=6);
+  
+  addDataFrame(x = as.data.frame.matrix(risk$`Sensitivity Given Specificity`), sheet = programBasedSheet, row.names=TRUE, col.name=TRUE);
+  addDataFrame(x = as.data.frame.matrix(risk$`Program-Based`), sheet = programBasedSheet, row.names=FALSE, col.name=TRUE, startColumn=6);
+  
+  addDataFrame(x = as.data.frame.matrix(risk$`Sensitivity Given Specificity`), sheet = ppvBasedSheet, row.names=TRUE, col.name=TRUE);
+  addDataFrame(x = as.data.frame.matrix(risk$`PPV-Based`), sheet = ppvBasedSheet, row.names=FALSE, col.name=TRUE, startColumn=6);
+  
+  addDataFrame(x = as.data.frame.matrix(risk$`Sensitivity Given Specificity`), sheet = sensitivityBasedSheet, row.names=TRUE, col.name=TRUE);
+  addDataFrame(x = as.data.frame.matrix(risk$`Sensitivity-Based`), sheet = sensitivityBasedSheet, row.names=FALSE, col.name=TRUE, startColumn=6);
+  
+  addDataFrame(x = as.data.frame.matrix(risk$`Sensitivity Given Specificity`), sheet = dominatedSheet, row.names=TRUE, col.name=TRUE);
+  addDataFrame(x = as.data.frame.matrix(risk$`Dominated by Specificity for a Rare Disease`), sheet = dominatedSheet, row.names=FALSE, col.name=TRUE, startColumn=6);
   
   addPicture(graphName, deltaSheet, scale = .65, startRow = 1, startColumn = 6);
 #   addPicture(paste(imageDirectory, "PerDeviations", uniqueId, ".png", sep = ''), perDeviationsSheet, scale = .65, startRow = 1, startColumn = 6);
