09e5e4113c6a7af3cb10de49e10241e82036ddb3
Sula Rajapakse <sulalith@gmail.com>
2014-05-21 17:50:36 -0400

    work done for genralizing the code to functions depending on the inputs

--- src/riskStratAdvanced/JsonWrapper.R
+++ src/riskStratAdvanced/JsonWrapper.R
@@ -1,19 +1,31 @@
 library('RJSONIO')
 library('stringr')
-source ('./RiskStratAnalysisOptions.R')
+source ('RiskStratAnalysisOptions.R')
 
 imageDirectory <- "./img/";
+rdirectory <- "";
+drawfunctionprefix = "Draw"
+keysforfunction = list("Sens"=1, "Spec"=2, "PPV"=3, "cNPV"=4, "Prev"=5, "Delta"=6);
+#keysforfunction = list("1"="Sens", "2"="Spec", "3"="PPV", "4"="cNPV", "5"="Prev", "6"="Delta");
+functionnames = list("sensitivity"="Sens", "specificity"="Spec", "ppv"="PPV", "cnpv"="cNPV", "prevalence"="Prev", "delta"="Delta");
+rfunctionname = ""
+
+inputtransposelist = list();
+datatransposed = "";
 
 getDataJSON <-function(urlEncodedString)
 {
+  dir.create(imageDirectory);
   inputList <- parseURLEncodedString(urlEncodedString);
-  contour<-getContour(inputList);
-  dppv<-getVector(contour);
-  fixed<-getFixedValue(inputList);
-  prev<-getVector(fixed);
-  dependent<-getDependent(inputList);
-  spec<-getVector(dependent);
-  key<-getKey(inputList);
+  independent<-getindependent(inputList);   #PPV -2
+  independentvalue<-getindependentValue(inputList);
+  independentVector<-getVector(getindependentValue(inputList));
+  fixed<-getfixed(inputList);   #PREV -3
+  fixedvalue<-getfixedValue(inputList);
+  fixedVector<-getVector(getfixedValue(inputList));
+  contour<- getcontour(inputList);   #SPEC -1
+  contourvalue<-getcontourValue(inputList);
+  contourVector<-getVector(getcontourValue(inputList));
   key<-getKey(inputList);
   specmin<-getSpecMin(inputList);
   specmax<-getSpecMax(inputList);
@@ -21,15 +33,45 @@ getDataJSON <-function(urlEncodedString)
   tab <- gettab(inputList);
   tabvalue <- gettabvalue(inputList);
   keyGraphName <- getgraphname(inputList);
-  data<- PPVPrevSpec(dppv,prev,spec);
+  #data<- PPVPrevSpec(dppv,prev,spec);
+  rfunctionname = getFunctionName(independent, fixed, contour);
+  rfunction <- get(rfunctionname, mode="function")
+  #data<- functionToCall(independentVector,fixedVector,contourVector);
+  functionvalueorder <- getfunctionvalueorder(getindependent(inputList), getfixed(inputList), getcontour(inputList), independentvalue, fixedvalue, contourvalue);
+  firstinputVector<-getVector(functionvalueorder[[1]]);
+  secondinputVector<-getVector(functionvalueorder[[2]]);
+  thirdinputVector<-getVector(functionvalueorder[[3]]);
+  data<- rfunction(firstinputVector,secondinputVector,thirdinputVector);
+  tranposeorder = "";
+  tranposeorder <- gettransposeorder(getindependent(inputList), getfixed(inputList), getcontour(inputList));
   jsonString = "";
 
+  # can be refactored to iterate through keys- table titles with the title mapping both in R and javascript
   if (key == "Sensitivity"){
-    jsonString=toJSON(data$"Sensitivity"[,,as.numeric(tab)], method="C");
-    getSensSpecGraph(as.numeric(specmin), as.numeric(specmax), as.numeric(tabvalue), dppv, keyGraphName, uniqueId);
+    datatransposed <- getTransposedData(getindependent(inputList), getfixed(inputList), getcontour(inputList), tranposeorder, data$"Sensitivity");
+    jsonString=toJSON(datatransposed[,,as.numeric(tab)], method="C");
+    drawfunctionname=getDrawFunctionName(drawfunctionprefix, key, rfunctionname);
+    drawfunction <- get(drawfunctionname, mode="function");
+    imageFileName = paste(imageDirectory, keyGraphName, uniqueId, "-", as.numeric(tabvalue), ".png", sep = '');
+    png(file = imageFileName , units="in", width=10, height=8, res=150);
+    returnvalue<- drawfunction(firstinputVector,secondinputVector,as.numeric(tabvalue));
+    #DrawDeltaSpecPPV(specmin, specmax, prev, ppv);
+    dev.off();
+    imageFileName;
+    
+    #getSensSpecGraph(as.numeric(specmin), as.numeric(specmax), as.numeric(tabvalue), dppv, keyGraphName, uniqueId);
   } else if (key == "Delta") {
-    jsonString=toJSON(data$"Delta"[,,as.numeric(tab)], method="C");
-    getDeltaSpecGraph(as.numeric(specmin), as.numeric(specmax), as.numeric(tabvalue), dppv, keyGraphName, uniqueId);
+    datatransposed <- getTransposedData(getindependent(inputList), getfixed(inputList), getcontour(inputList), tranposeorder, data$"Delta");
+    jsonString=toJSON(datatransposed[,,as.numeric(tab)], method="C");
+    drawfunctionname=getDrawFunctionName(drawfunctionprefix, key, rfunctionname);
+    drawfunction <- get(drawfunctionname, mode="function");
+    imageFileName = paste(imageDirectory, keyGraphName, uniqueId, "-", as.numeric(tabvalue), ".png", sep = '');
+    png(file = imageFileName , units="in", width=10, height=8, res=150);
+    returnvalue<- drawfunction(firstinputVector,secondinputVector,as.numeric(tabvalue));
+    #DrawDeltaSpecPPV(specmin, specmax, prev, ppv);
+    dev.off();
+    imageFileName;
+    #getDeltaSpecGraph(as.numeric(specmin), as.numeric(specmax), as.numeric(tabvalue), dppv, keyGraphName, uniqueId);
   } else if (key == "Prevalence_Odds_Length") {
     jsonString=toJSON(length(data$"Prevalence_Odds"), method="C");
   }
@@ -43,6 +85,94 @@ getVector <- function (vectorstring) {
      returnvector<-append(returnvector, as.numeric(i));
    };
    return(returnvector);
+}
+
+getFunctionName <- function (i, j, k) {
+  tranposelist = list();
+  rFileName = paste(rdirectory);
+  inputnames = list("independent"=i, "fixed"=j, "contour"=k)
+  for (name in names(functionnames))
+  {
+    for (variablename in names(inputnames))
+    {
+        print(inputnames[[variablename]])
+        if (name == inputnames[[variablename]])
+        {
+           rFileName = paste(rFileName, functionnames[[name]], sep = "")
+           #tranposelist = append(name, tranposelist)
+           tranposelist <- c(tranposelist, name)
+        }
+    }
+  }
+  print(rFileName)
+  print(tranposelist)
+  return(rFileName)
+}
+
+getDrawFunctionName <- function (drawfunctionprefix, key, rfunctionname) {
+  rDrawFileName = paste(rdirectory);
+  rDrawFileName = paste(rDrawFileName, drawfunctionprefix, functionnames[[tolower(c(key))]], rfunctionname, sep = "")
+
+  print(rDrawFileName)
+  print(rfunctionname)
+  return(rDrawFileName)
+}
+
+gettransposeorder <- function (independent, fixed, contour) {
+  tranposelist = list();
+  inputnames = list("independent"=independent, "fixed"=fixed, "contour"=contour)
+  for (name in names(keysforfunction))
+  {
+    for (variablename in names(inputnames))
+    {
+      if (name == functionnames[[inputnames[[variablename]]]])
+      {
+        tranposelist <- c(tranposelist, name)
+      }
+    }
+  }
+  print(tranposelist)
+  return(tranposelist)
+}
+
+getfunctionvalueorder <- function (independent, fixed, contour, independentvalue, fixedvalue, contourvalue)
+{
+  tranposevaluelist = list();
+  tranposelist = list();
+  inputnames = list("independent"=independent, "fixed"=fixed, "contour"=contour)
+  inputvalues = list("independent"=independentvalue, "fixed"=fixedvalue, "contour"=contourvalue)
+  for (name in names(keysforfunction))
+  {
+    for (variablename in names(inputnames))
+    {
+      if (name == functionnames[[inputnames[[variablename]]]])
+      {
+        tranposevaluelist <- c(tranposevaluelist, inputvalues[[variablename]])
+        tranposelist <- c(tranposelist, name)
+      }
+    }
+  }
+  print(tranposevaluelist)
+  print(tranposelist)
+  return(tranposevaluelist)
+}
+
+getTransposedData <- function (independent, fixed, contour, tranposeorder, dataMatrix)
+{
+  dataInputOrder = list("independent"=independent, "fixed"=fixed, "contour"=contour)
+  for (i in 1:3)
+  {
+    for (variablename in names(dataInputOrder))
+    {
+        if (tranposeorder[[i]] == functionnames[[dataInputOrder[[variablename]]]])
+              inputtransposelist[[variablename]] <- i
+    }
+  }
+  print(inputtransposelist[["independent"]])
+  print(inputtransposelist[["contour"]])
+  print(inputtransposelist[["fixed"]])
+  datax <- aperm(dataMatrix, c(inputtransposelist[["independent"]],inputtransposelist[["contour"]],inputtransposelist[["fixed"]]))
+  return (datax)
 }
 
 getDeltaSpecGraph <- function (specmin, specmax, prev, ppv, keyGraphName, uniqueId) {
@@ -65,40 +195,52 @@ getKey <- function (inputList) {
   inputList[[1]][[1]];
 }
 
-getContour <- function (inputList) {
+getindependent <- function (inputList) {
   inputList[[2]][[1]];
 }
 
-getFixedValue <- function (inputList) {
+getfixed <- function (inputList) {
   inputList[[3]][[1]];
 }
 
-getDependent <- function (inputList) {
+getcontour <- function (inputList) {
   inputList[[4]][[1]];
 }
 
-getSpecMin <- function (inputList) {
+getindependentValue <- function (inputList) {
   inputList[[5]][[1]];
 }
 
-getSpecMax <- function (inputList) {
+getfixedValue <- function (inputList) {
   inputList[[6]][[1]];
 }
 
-getUniqueId <- function (inputList) {
+getcontourValue <- function (inputList) {
   inputList[[7]][[1]];
 }
 
-gettab <- function (inputList) {
+getSpecMin <- function (inputList) {
   inputList[[8]][[1]];
 }
 
-gettabvalue <- function (inputList) {
+getSpecMax <- function (inputList) {
   inputList[[9]][[1]];
 }
 
-getgraphname <- function (inputList) {
+getUniqueId <- function (inputList) {
   inputList[[10]][[1]];
+}
+
+gettab <- function (inputList) {
+  inputList[11][[1]];
+}
+
+gettabvalue <- function (inputList) {
+  inputList[[12]][[1]];
+}
+
+getgraphname <- function (inputList) {
+  inputList[[13]][[1]];
 }
 
 JsonWrapper <- function(dppv,prev,spec)
--- src/riskStratAdvanced/RiskStratAnalysisOptions.R
+++ src/riskStratAdvanced/RiskStratAnalysisOptions.R
@@ -637,6 +637,8 @@ SenscNPVSpec <- function(sens,dcnpv,spec) {
 }
 
 
+
+
 ###
 ## Different drawing functions for independent vs dependent with different contours
 ##
@@ -2272,4 +2274,24 @@ DrawSensSpecPPVPrev <- function(spec, PPV, prevalence) {
   }
   
   legend(lty=LTY,legend=PPV,"bottomright",cex=1.15,text.font=2,bty="o",inset=c(-0.4,0),xpd = TRUE,pch=c(1,3),title="PPV")
+}
+
+DrawDeltaSpecPPVPrev <- function(spec, PPV, prevalence) {
+  #Specificity <- seq(from=specmin,to=specmax,by=0.00001)
+  Specificity <- seq(from=min(spec),to=max(spec),by=0.00001)
+  
+  Iterations <- 1:length(PPV)
+  LTY <- Iterations
+  
+  for(i in Iterations){
+    Delta <- qnorm(Specificity) - qnorm(1-(PPV[i]/(1-PPV[i]))/(prevalence/(1-prevalence))*(1-Specificity))
+    if(i==1) {
+      par(mar=c(5.1, 4.1, 4.1, 9.5))
+      plot(Delta~Specificity,type="l",main=c("Delta vs. Specificity","Given Different Values of Positive Predictive Value",paste("Prevalence =",prevalence)),xlab="Specificity",ylab="Delta",ylim=c(min(Delta),5),lty=LTY[i],font.lab=2,font=2,cex.axis=1.15,cex.lab=1.15)
+    }
+    else
+      lines(y=Delta, x=Specificity,lty=LTY[i])
+  }
+  
+  legend(lty=LTY,legend=PPV,"bottomright",cex=1.15,text.font=2,bty="o",inset=c(-0.4,0),xpd = TRUE,pch=c(1,3),title="PPV") 
 }
