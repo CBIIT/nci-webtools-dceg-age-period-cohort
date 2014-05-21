8f6ccfb3ccb1f3d0583deb190912fa3c40cdf555
suepan <pansu@mail.nih.gov>
2014-05-21 18:22:21 -0400

    added missing drawing function

--- src/riskStratAdvanced/RiskStratAnalysisOptions.R
+++ src/riskStratAdvanced/RiskStratAnalysisOptions.R
@@ -780,6 +780,25 @@ DrawDeltaPPVSpecPrev <- function(ppv,  specificity, prevalence) {
   
 }
 
+DrawDeltaSpecPPVPrev <- function(specificity, ppv, prevalence) {
+  Specificity <- seq(from=min(specificity),to=max(specificity),by=0.00001)
+  
+  Iterations <- 1:length(ppv)
+  LTY <- Iterations
+  
+  for(i in Iterations){
+    Delta <- qnorm(Specificity) - qnorm(1-(ppv[i]/(1-ppv[i]))/(prevalence/(1-prevalence))*(1-Specificity))
+    if(i==1) {
+      par(mar=c(5.1, 4.1, 4.1, 9.5))
+      plot(Delta~Specificity,type="l",main=c("Delta vs. Specificity","Given Different Values of Positive Predictive Value",paste("Prevalence =",prevalence)),xlab="Specificity",ylab="Delta",ylim=c(min(Delta),5),lty=LTY[i],font.lab=2,font=2,cex.axis=1.15,cex.lab=1.15)
+    }
+    else
+      lines(y=Delta, x=Specificity,lty=LTY[i])
+  }
+  
+  legend(lty=LTY,legend=ppv,"bottomright",cex=1.15,text.font=2,bty="o",inset=c(-0.4,0),xpd = TRUE,pch=c(1,3),title="PPV") 
+}
+
 DrawPPVSpecDeltaPrev <- function(spec, delta, prevalence) {
   
   Val <- prevalence/(1-prevalence)
