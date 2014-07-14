cfb15c9f623a69020271c49ff3b449b9e376e644
suepan <pansu@mail.nih.gov>
2014-07-14 12:19:35 -0400

    add creation of image directory

--- src/apc/apcForRpy2.R
+++ src/apc/apcForRpy2.R
@@ -135,6 +135,7 @@ createPanCanList <- function (inputList) {
 }
 
 getGraph <- function (apcOutput, keyGraphName, uniqueId) {
+  dir.create(imageDirectory);
   imageFileName = paste(imageDirectory, keyGraphName, uniqueId, ".png", sep = '');
   png(file = imageFileName , units="in", width=10, height=8, res=150);
   line.apc(apcOutput, keyGraphName);
