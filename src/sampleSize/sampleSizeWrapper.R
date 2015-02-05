81a57a48784c7c64192c9cfdc5d8204935d2bed3
suepan <pansu@NCI-01962465-L.nci.nih.gov>
2015-02-05 15:02:39 -0500

    updated to have functions that can be called from Python

--- src/sampleSize/sampleSizeWrapper.R
+++ src/sampleSize/sampleSizeWrapper.R
@@ -1,45 +1,50 @@
 source ('./DrawCompRecVark.R')
 
-#example input values
-k=c(0,1)
-sens=c(0.8, 0.9, 0.95, 0.995)
-spec=0.8
-prev=0.001
-N=1
-
 imageDirectory="./tmp/" 
-uniqueId=1
-dir.create(imageDirectory)
-
-#save the PPV graph
-ppvGraph = paste(imageDirectory, "PPVkSensSpec-", uniqueId, ".png", sep = '')
-png(file=ppvGraph)
-DrawCompRecVarkSensSpec(k, sens, spec, prev, N)
-dev.off();
-
-#save the cNPV graph
-cnpvGraph = paste(imageDirectory, "cNPVkSensSpec-", uniqueId, ".png", sep = '')
-png(file=cnpvGraph)
-DrawCompRecVarcNPVkSensSpec(k, sens, spec, prev, N)
-dev.off();
 
 #example input values
-k=c(0,1)
-spec=c(0.8, 0.9, 0.95, 0.995)
-sens=0.1
-prev=0.001
-N=1
+#k=c(0,1)
+#sens=c(0.8, 0.9, 0.95, 0.995)
+#spec=0.8
+#prev=0.001
+#N=1
 
-uniqueId=2
+saveSensContours <- function(k, sens, spec, prev, N, uniqueId)
+{
+  #save the PPV graph
+  prepareSaveGraph(imageDirectory, "PPVkSensSpec-", uniqueId)
+  DrawCompRecVarkSensSpec(k, sens, spec, prev, N)
+  dev.off()
+  
+  #save the cNPV graph
+  prepareSaveGraph(imageDirectory, "cNPVkSensSpec-", uniqueId)
+  DrawCompRecVarcNPVkSensSpec(k, sens, spec, prev, N)
+  dev.off()
+}
 
-#save the PPV graph
-ppvGraph = paste(imageDirectory, "PPVkSpecSens-", uniqueId, ".png", sep = '')
-png(file=ppvGraph)
-DrawCompRecVarkSpecSens(k, spec, sens, prev, N)
-dev.off();
+#example input values
+#k=c(0,1)
+#spec=c(0.8, 0.9, 0.95, 0.995)
+#sens=0.1
+#prev=0.001
+#N=1
 
-#save the cNPV graph
-cnpvGraph = paste(imageDirectory, "cNPVkSpecSens-", uniqueId, ".png", sep = '')
-png(file=cnpvGraph)
-DrawCompRecVarcNPVkSpecSens(k, spec, sens, prev, N)
-dev.off();
+saveSpecContours <- function(k, sens, spec, prev, N, uniqueId)
+{
+  #save the PPV graph
+  prepareSaveGraph(imageDirectory, "PPVkSpecSens-", uniqueId)
+  DrawCompRecVarkSensSpec(k, sens, spec, prev, N)
+  dev.off()
+  
+  #save the cNPV graph
+  prepareSaveGraph(imageDirectory, "cNPVkSpecSens-", uniqueId)
+  DrawCompRecVarcNPVkSensSpec(k, sens, spec, prev, N)
+  dev.off();
+}
+
+#preparation needed to save a graph as a file
+prepareSaveGraph <- function(imgDir, graphPrefix, uniqueId) {
+  dir.create(imgDir)
+  graph=paste(imgDir, graphPrefix, uniqueId, ".png", sep='')
+  png(file=graph)
+}
