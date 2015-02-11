da41c8ef8d401f455fadc62b6dc366807898c00d
unknown <cookhj@NCI-01979367-L.nci.nih.gov>
2015-02-11 16:53:01 -0500

    Beta

--- src/sampleSize/sampleSizeWrapper.R
+++ src/sampleSize/sampleSizeWrapper.R
@@ -9,15 +9,22 @@ imageDirectory="./tmp/"
 #prev=0.001
 #N=1
 
-saveSensContours <- function(k, sens, spec, prev, N, uniqueId)
+#saveSensContours <- function()
+saveSensContours <- function(k, sens, spec, prev, N, uniqueId, tabvalue)
 {
+  #k=c(0,1)
+  #sens=c(0.8, 0.9, 0.95, 0.995)
+  #spec=0.8
+  #prev=0.001
+  #N=1
+  #uniqueId=1
   #save the PPV graph
-  prepareSaveGraph(imageDirectory, "PPVkSensSpec-", uniqueId)
+  prepareSaveGraph(imageDirectory, "PPVkSensSpec-", uniqueId, tabvalue)
   DrawCompRecVarkSensSpec(k, sens, spec, prev, N)
   dev.off()
   
   #save the cNPV graph
-  prepareSaveGraph(imageDirectory, "cNPVkSensSpec-", uniqueId)
+  prepareSaveGraph(imageDirectory, "cNPVkSensSpec-", uniqueId, tabvalue)
   DrawCompRecVarcNPVkSensSpec(k, sens, spec, prev, N)
   dev.off()
 }
@@ -29,22 +36,22 @@ saveSensContours <- function(k, sens, spec, prev, N, uniqueId)
 #prev=0.001
 #N=1
 
-saveSpecContours <- function(k, sens, spec, prev, N, uniqueId)
+saveSpecContours <- function(k, sens, spec, prev, N, uniqueId, tabvalue)
 {
   #save the PPV graph
-  prepareSaveGraph(imageDirectory, "PPVkSpecSens-", uniqueId)
-  DrawCompRecVarkSensSpec(k, sens, spec, prev, N)
+  prepareSaveGraph(imageDirectory, "PPVkSpecSens-", uniqueId, tabvalue)
+  DrawCompRecVarkSpecSens(k, spec, sens, prev, N)
   dev.off()
   
   #save the cNPV graph
-  prepareSaveGraph(imageDirectory, "cNPVkSpecSens-", uniqueId)
-  DrawCompRecVarcNPVkSensSpec(k, sens, spec, prev, N)
+  prepareSaveGraph(imageDirectory, "cNPVkSpecSens-", uniqueId, tabvalue)
+  DrawCompRecVarcNPVkSpecSens(k, spec, sens, prev, N)
   dev.off();
 }
 
 #preparation needed to save a graph as a file
-prepareSaveGraph <- function(imgDir, graphPrefix, uniqueId) {
+prepareSaveGraph <- function(imgDir, graphPrefix, uniqueId, tabvalue) {
   dir.create(imgDir)
-  graph=paste(imgDir, graphPrefix, uniqueId, ".png", sep='')
+  graph=paste(imgDir, graphPrefix, uniqueId, "-", as.numeric(tabvalue),".png", sep='')
   png(file=graph)
 }
