e72b43dec79c3185f31ee076b3cfd117928aca9c
suepan <pansu@NCI-01962465-L.nci.nih.gov>
2015-02-12 14:18:36 -0500

    updated the initial example to be a function

--- src/jpsurv/JPSurvWrapper.R
+++ src/jpsurv/JPSurvWrapper.R
@@ -1,16 +1,21 @@
 library('RJSONIO');
 library('JPSurv');
 
-# Make sure the working directory is set to where the dictionary file is saved.
 
-dictionary = dictionary.overview("C:/devel/R/Breast_RelativeSurvival.dic");
+getDictionaryAsJson <- function (fullPathDictionaryFile) {
+  #e.g.
+  #dictionary = dictionary.overview("C:/devel/R/Breast_RelativeSurvival.dic");
+  dictionary = dictionary.overview(fullPathDictionaryFile)
 
-# DICsample is a list of the various sections of the .dic file. The names of the sections can be found by expanding 
-# the object in the Environment tab in R Studio.
-# For your use I think you will be interested in: VarAllInfo, VarFormatSecList, VarLabelInfo.
+  #translate to JSON as is
+  dictionaryJSON=cat(toJSON(dictionary), .escapeEscapes=TRUE)
+  
+  #transpose so that the var names are the column headers
+  #varInfo=setNames(data.frame(t(dictionary$VarAllInfo[, -1])), dictionary$VarAllInfo[,1])
+  #this is not being used right now
+  
+  return(dictionaryJSON)
+}
 
-#variable info 
-#transpose so that the var names are the column headers
-#varInfo=setNames(data.frame(t(dictionary$VarAllInfo[, -1])), dictionary$VarAllInfo[,1])
 
-dictionaryJSON=cat(toJSON(dictionary), .escapeEscapes=TRUE)
+
