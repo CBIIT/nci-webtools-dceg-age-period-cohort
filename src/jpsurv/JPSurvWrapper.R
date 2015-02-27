d3782838835f67d5c8c0bff59ccb670aafe0e840
suepan <pansu@NCI-01962465-L.nci.nih.gov>
2015-02-27 17:25:33 -0500

    removed testing variables

--- src/jpsurv/JPSurvWrapper.R
+++ src/jpsurv/JPSurvWrapper.R
@@ -137,15 +137,3 @@ getFittedResult <- function (filePath, seerFilePrefix, yearOfDiagnosisVarName, y
   apcJson=cat(toJSON(fit.result$apc), .escapeEscapes=TRUE)
   return (apcJson)
 }
-
-
-filePath="C:/devel/R"
-seerFilePrefix="Breast_RelativeSurvival"
-yearOfDiagnosisVarName="Year_of_diagnosis_1975"
-yearofDiagnosisRange=c(1975, 2011)
-allVar=c("Age_groups","Breast_stage","Year_of_diagnosis_1975")
-cohortVars=c("Age_groups")
-cohortValues=c("\"65+\"")
-covariateVars=c("Breast_stage") 
-numJP=1
-outputFileName="Breast_RelativeSurvival.output"
