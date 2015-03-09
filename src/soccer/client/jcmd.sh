183dedda10ccc16369158d28fde0603e7c3fe163
yutaowang <NCI-01979370-L+wangy21@NCI-01979370-L.nci.nih.gov>
2015-03-08 23:04:13 -0400

    modified command path

--- src/soccer/client/jcmd.sh
+++ src/soccer/client/jcmd.sh
@@ -2,8 +2,7 @@
 
 if [ -e "$1" ]
 then
-java -cp "lib/*" -Dgov.nih.cit.soccer.wordnet.dir="/local/content/soccer/dict" -Dgov.nih.cit.soccer.output.dir="/usr/local/tomee/webapps/soccer/files" gov.nih.cit.soccer.Soccer $1
+java -cp "lib/*" -Dgov.nih.cit.soccer.wordnet.dir="/local/content/soccer/dict" -Dgov.nih.cit.soccer.output.dir="/local/content/tomee/webapps/soccer/files" gov.nih.cit.soccer.Soccer $1
 else
 echo "$1 does not exist!"
 fi
-
