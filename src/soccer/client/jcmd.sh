46fec54b2137718e018adfbfbf4faf0f4e43e00d
suepan <pansu@mail.nih.gov>
2015-04-14 23:03:46 -0400

    split to a separate repo

--- src/soccer/client/jcmd.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-if [ -e "$1" ]
-then
-java -cp "lib/*" -Dgov.nih.cit.soccer.wordnet.dir="/local/content/soccer/dict" -Dgov.nih.cit.soccer.output.dir="/local/content/tomee/webapps/soccer/files" gov.nih.cit.soccer.Soccer $1
-else
-echo "$1 does not exist!"
-fi
