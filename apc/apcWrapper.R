be01728a5be19cb45bfb3c38916647deb4438dc4
Brian Park <re.public@outlook.com>
2016-01-05 11:36:37 -0500

    added comments to wrapper

--- apc/apcWrapper.R
+++ apc/apcWrapper.R
@@ -21,7 +21,7 @@ graphKeys = c("AgeDeviations",
               "LocalDrifts")
 
 ## Test data generated from: example_data/ClaytonSchifflers1987StatMed.csv
-## testData = '{"title":"Belgium Female Lung Cancer Mortality","description":"Example from: Clayton D. & Schifflers E. Models for temporal variation in cancer rates. I: Age-period and age-cohort models. Stat. Med., 1987; 6:449-467.","startYear":1955,"startAge":25,"interval":5,"count":[[3,2,7,3,10],[11,16,11,10,7],[11,22,24,25,15],[36,44,42,53,48],[77,74,68,99,88],[106,131,99,142,134],[157,184,189,180,177],[193,232,262,249,239],[219,267,323,325,343],[223,250,308,412,358],[198,214,253,338,312]],"population":[[1578947.368,1538461.538,1400000,1578947.368,1428571.429],[1666666.667,1632653.061,1527777.778,1408450.704,1228070.175],[1410256.41,1666666.667,1632653.061,1524390.244,1136363.636],[1348314.607,1392405.063,1660079.051,1568047.337,1221374.046],[1590909.091,1321428.571,1379310.345,1636363.636,1288433.382],[1606060.606,1541176.471,1294117.647,1340887.63,1285988.484],[1515444.015,1533333.333,1490536.278,1255230.126,986072.4234],[1307588.076,1417226.634,1455555.556,1414772.727,999581.765],[1066731.612,1181415.929,1297188.755,1335799.425,1048929.664],[849847.561,902527.0758,1010830.325,1115322.144,930595.269],[591574.5444,636715.2633,688060.9192,773632.4102,690265.4867]],"refYear":-1,"refAge":-1,"refCohort":-1}'
+## testData = '{"title":"Belgium Female Lung Cancer Mortality","description":"Example from: Clayton D. & Schifflers E. Models for temporal variation in cancer rates. I: Age-period and age-cohort models. Stat. Med., 1987; 6:449-467.","startYear":1955,"startAge":25,"interval":5,"table":[[3,1578947.368,2,1538461.538,7,1400000,3,1578947.368,10,1428571.429],[11,1666666.667,16,1632653.061,11,1527777.778,10,1408450.704,7,1228070.175],[11,1410256.41,22,1666666.667,24,1632653.061,25,1524390.244,15,1136363.636],[36,1348314.607,44,1392405.063,42,1660079.051,53,1568047.337,48,1221374.046],[77,1590909.091,74,1321428.571,68,1379310.345,99,1636363.636,88,1288433.382],[106,1606060.606,131,1541176.471,99,1294117.647,142,1340887.63,134,1285988.484],[157,1515444.015,184,1533333.333,189,1490536.278,180,1255230.126,177,986072.4234],[193,1307588.076,232,1417226.634,262,1455555.556,249,1414772.727,239,999581.765],[219,1066731.612,267,1181415.929,323,1297188.755,325,1335799.425,343,1048929.664],[223,849847.561,250,902527.0758,308,1010830.325,412,1115322.144,358,930595.269],[198,591574.5444,214,636715.2633,253,688060.9192,338,773632.4102,312,690265.4867]],"refYear":-1,"refAge":-1,"refCohort":-1,"cohort":-1}'
 ## Generate output with: getApcDataJSON(testData)
 
 #-------------------------------------------------------
@@ -43,15 +43,15 @@ getApcDataJSON <- function(jsonData) {
   # Parse output data to results
   results = list()
 
-  ## Generate Tables
+  ## Retrieve tables
   for (key in c(keys, graphKeys))
     results[[key]]$table = as.data.frame(output[[key]])
 
-  ## Generate Graphs
+  ## Generate graphs
   for (key in graphKeys)
     results[[key]]$pathToFile = getGraph(output, key)
 
-  ## Generate Raw Input/Output Files
+  ## Generate raw input/output files
   results $RDataInput  $pathToFile = paste0(tmpDirectory, "APCAnalysis_", getTimestamp() , "_RawInput.RData")
   results $RDataOutput $pathToFile = paste0(tmpDirectory, "APCAnalysis_", getTimestamp() , "_RawOutput.RData")
   results $TextInput   $pathToFile = paste0(tmpDirectory, "APCAnalysis_", getTimestamp() , "_RawInput.txt")
@@ -62,10 +62,10 @@ getApcDataJSON <- function(jsonData) {
   capture.output(print(input),  file = results $TextInput  $pathToFile)
   capture.output(print(output), file = results $TextOutput $pathToFile)
   
-  ## Generate Excel Spreadsheet
+  ## Generate excel spreadsheet
   results $Excel $pathToFile = generateExcel(results, input$name)
 
-  return (toJSON(results))
+  toJSON(results)
 }
 
 
@@ -84,13 +84,16 @@ parseJSON <- function(jsonData) {
   endAge        = data$startAge  + data$interval * nrow(table)
   endYear       = data$startYear + data$interval * ncol(table) / 2
   
+  # Add default title and description if none were provided
+
   if (data$title == '')
     data$title = 'APC Analysis'
   
   if (data$description == '')
     data$description = paste('Timestamp: ', getTimestamp())
   
-  contents = list(
+  # Return a list that can be used for the apc calculation 
+  list(
     name        = data$title,
     description = data$description,
     events      = as.matrix(table[sequence == 1]), # odd numbered columns
@@ -132,7 +135,7 @@ runAPC <- function(jsonData) {
 getGraph <- function (apcOutput, keyGraphName) {
   imageFileName = paste0(tmpDirectory, keyGraphName, "_", getTimestamp(), ".png")
   
-  png(file = imageFileName , units="px", width=600, height=400, res=72, pointsize = 10)
+  png(file = imageFileName , units="px", width=600, height=500, res=72, pointsize = 10)
   line.apc(apcOutput, keyGraphName)
   dev.off()
   
@@ -162,19 +165,22 @@ generateExcel <- function(apcOutput, title) {
   
   workbook = createWorkbook()
   
-  ## Create Sheets
+  ## Iterate through the list of keys
   for (key in c(graphKeys, keys)) {
     
+    # Create a sheet and set it as the current working sheet
     currentSheet = createSheet(workbook, sheetName = key)
     
+    # These tables have row names
     if (key %in%  c("Waldtests", "Coefficients"))
       addDataFrame(x = round(apcOutput[[key]]$table, 3), sheet = currentSheet)
     
     else
       addDataFrame(x = round(apcOutput[[key]]$table, 3), sheet = currentSheet, row.names = FALSE)
     
+    # If this key has a graph, add it to the current sheet
     if (key %in% graphKeys)
-      addPicture(apcOutput[[key]]$pathToFile, currentSheet, scale = .65, startRow = 1, startColumn = 6)
+      addPicture(apcOutput[[key]]$pathToFile, currentSheet, startRow = 1, startColumn = 6)
   }
 
   pathToFile = paste0(tmpDirectory, title, " ", getTimestamp(), ".xlsx")
