958e9fbedb687e067671d437a24100609f05b1fc
unknown <zhoujim@NCI-01979649-L.nci.nih.gov>
2016-03-08 12:32:14 -0500

    Added external configuration to enable debugging

--- apc/apc.py
+++ apc/apc.py
@@ -13,16 +13,13 @@ import argparse
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument("-p", dest="port_number", default="9040", help="Sets the Port") 
-    parser.add_argument("-d", dest="debug", default="False", help="Sets the Debugging Option") 
     # Default port is production value; prod, stage, dev = 8040, sandbox = 9040
     args = parser.parse_args()
     port_num = int(args.port_number)
     
-    if args.debug == 'True' :
-    	debug = True
-    else :
-    	debug = False
-    
+    with open('apc.conf', 'r') as f:
+    	debug = f.read().strip().split('=')[1] == 'True'
+    f.close()
 
     hostname = gethostname()
     app.run(host='0.0.0.0', port=port_num, debug = debug) 
