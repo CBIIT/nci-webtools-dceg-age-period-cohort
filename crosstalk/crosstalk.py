e168dbed1f5d92e683493dcfc41abe4dca1f76be
unknown <zhoujim@NCI-01979649-L.nci.nih.gov>
2016-04-04 10:45:31 -0400

    Change code to make it work first

--- crosstalk/crosstalk.py
+++ crosstalk/crosstalk.py
@@ -3,19 +3,20 @@ from socket import gethostname
 import rpy2.robjects as robjects
 
 app = Flask(__name__)
-robjects.r['source']('CrossTalkWrapper.R')
+#robjects.r['source']('CrossTalkWrapper.R')
 
 @app.route('/crossTalkRest/', methods = ['POST'])
 def calculation():
-    return robjects.r['getCrossTalkDataJSON'](request.stream.read())[0]
+    return '' 
+    #robjects.r['getCrossTalkDataJSON'](request.stream.read())[0]
 
 import argparse
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument("-p", dest="port_number", default="9040", help="Sets the Port") 
-    # Default port is production value; prod, stage, dev = 8040, sandbox = 9040
+    parser.add_argument("-p", dest="port_number", default="9140", help="Sets the Port") 
+    # Default port is production value; prod, stage, dev = 8140, sandbox = 9140
     args = parser.parse_args()
     port_num = int(args.port_number)
 
     hostname = gethostname()
-    app.run(host='0.0.0.0', port=port_num, debug = True) 
+    app.run(host='0.0.0.0', port=port_num, debug=True, use_evalex=False) 
