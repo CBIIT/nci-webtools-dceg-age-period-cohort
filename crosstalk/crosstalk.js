1125084361dfd36437541b791632bd135b188463
Wesley Obenshain <umdshaman@gmail.com>
2016-04-18 05:43:05 -0400

    missed the moved file. Merged better

--- crosstalk/crosstalk.js
+++ crosstalk/crosstalk.js
@@ -1,5 +1,4 @@
 $(document).ready(function () {
-
     // Save DOM references to minimize queries
     var cfg = crosstalk.config({
         description: $('#description'),
@@ -15,6 +14,38 @@ $(document).ready(function () {
     // Add event listeners
     for (element in cfg)
         cfg[element].change(crosstalk.update);
+
+    $.adamant.pastable.functions["crosstalk-input"] = function(target,data) {
+        var data = data.split(/[\r\n]+/);
+        while (data[data.length-1].trim() == "") {data.pop();}
+        data = data.map(function(line) {
+            var line = line.split(",");
+            line.map(function(entry) {
+                return parseFloat(entry);
+            });
+            return line;
+        });
+        var datatarget = $(target).attr('data-target');
+        self.model[datatarget] = $.extend(self.model[datatarget]||{},{'table': data, 'title': $('#'+$('#'+datatarget).attr('data-target')).val()});
+        $(target).children("textarea").val("");
+        crosstalk.update();
+    };
+    
+    $('#dataset1, #dataset2').on('drop',function(e) {
+      e.preventDefault();
+      crosstalk.drop(e);
+    });
+
+    $("#dataFlip").click(function(e) {
+      e.preventDefault();
+      var swap = self.model.inputfile2;
+      self.model.inputfile2 = self.model.inputfile1;
+      self.model.inputfile1 = swap;
+      swap = cfg.titleB.val();
+      cfg.titleB.val(cfg.titleA.val());
+      cfg.titleA.val(swap);
+      crosstalk.update();
+    });
 
     $('#startYear').spinner({
         min: 1800,
@@ -63,6 +94,7 @@ $(document).ready(function () {
     $('#modelBt').click(crosstalk.getData);
 
     $('[type="reset"]').click(crosstalk.reset);
+    crosstalk.reset();
 });
 
 /* ---------- ReadFile Module - Parses an input file as a crosstalk model ---------- */
@@ -96,13 +128,14 @@ var readfile = (function () {
                 reader.readAsText(file);
 
             reader.onload = function (event) {
-                callback(event.currentTarget.result.match(/[^\r\n]+/g));
+                callback(event.currentTarget.result.split(/[\r\n]+/g));
             };
         }
     }
 
     /* ---------- Parse an array of lines (from a csv) as a matrix of floats ---------- */
     function parseTable(table) {
+        while (table[table.length-1].trim() == "") {table.pop();}
         return table.map(function (line) {
             return line.split(',').map(function (element) {
                 return parseFloat(element);
@@ -167,6 +200,7 @@ var crosstalk = (function ($, ReadFile) {
 
     return {
         calculate: calculate,
+        drop: drop,
         update: update,
         config: config,
         log: log,
@@ -319,19 +353,34 @@ var crosstalk = (function ($, ReadFile) {
         console.log('JSON object for calculations: ' + self.model);
     }
 
+    function drop(e) {
+        ReadFile.createModel({"id": $(e.delegateTarget).attr('data-target'), "files": e.originalEvent.dataTransfer.files}, updateModel);
+    }
+    
     /* ---------- Updates the UI and Model ---------- */
     function update() {
         var self = this;
 
         // Updates the model based on the contents of the file
-        if (self.type == 'file')
+        if (self.type == 'file') {
             ReadFile.createModel(self, updateModel);
-        else
+            var target = $('#inputfile1, #inputfile2');
+            target.wrap("<form>").closest("form")[0].reset();
+            target.unwrap();
+        } else {
             syncModel();
+        }
     }
 
-    function reset() {
+    function reset(e) {
+        if (e !== undefined) e.preventDefault();
         $(".graphsContainers").empty();
+        $('#ratePane').attr('style', 'display:none');
+        $('#showNumber').attr('style', 'display:none');
+        $('#showPlot').attr('style', 'display:none');
+        $('#apcRatePane').attr('style', 'display:none');
+        $('#apcRatioPane').attr('style', 'display:none');
+        $('#description,#startAge,#startYear,#interval,#title1,#title2').val("");
         $(".tablesContainers table").each(function () {
             if ($.fn.DataTable.isDataTable(this)) {
                 $(this).DataTable().destroy();
@@ -339,22 +388,81 @@ var crosstalk = (function ($, ReadFile) {
             $(this).empty();
             $("h4.title").empty();
         });
+        var matrix = [];
+        for (var i = 0; i < 13; i++) {
+            var arr = [];
+            for (var j = 0; j < 7; j++) {
+                arr.push("&zwj;");
+            }
+            matrix.push(arr);
+        }
+        createInputTable("#dataset1",createHeaders(3),matrix);
+        createInputTable("#dataset2",createHeaders(3),matrix);
+        $("#dataset1 .paste-here, #dataset2 .paste-here").remove();
+        $("#dataset1 textarea, #dataset2 textarea").before('<img class="img-responsive paste-here" alt="paste here" src="/common/images/paste-here.gif"/>');
     }
 
     /* ---------- Updates the model with contents from the UI ---------- */
     function syncModel() {
         self.model.description = self.cfg.description.val();
-        self.model.startYear = self.cfg.startYear.val();
-        self.model.startAge = self.cfg.startAge.val();
-        self.model.interval = self.cfg.interval.val();
+        self.model.startYear = parseFloat(self.cfg.startYear.val());
+        self.model.startAge = parseFloat(self.cfg.startAge.val());
+        self.model.interval = parseFloat(self.cfg.interval.val());
         self.model.titleA = self.cfg.titleA.val();
         self.model.titleB = self.cfg.titleB.val();
+        if (self.model.inputfile1) {
+            self.model.inputfile1.title = self.model.titleA;
+            table_input($("#dataset1"),self.model.inputfile1);
+        }
+        if (self.model.inputfile2) {
+            self.model.inputfile2.title = self.model.titleB;
+            table_input($("#dataset2"),self.model.inputfile2);
+        }
     }
+
+    function create_line(line,i) {
+      var lead = "&zwj;";
+      if (self.model.startAge && self.model.interval) {
+        var age = self.model.startAge+(self.model.interval*i);
+        lead = age + "-" + (age+self.model.interval-1);
+      }
+      line.unshift(lead);
+      return line;
+    };
+
+    function table_input(target,data) {
+        var target = $(target);
+        var tableData = $.extend(true,[],data.table);
+        if (tableData.length < 1) {
+            alert('needs a message 1');
+            return;
+        }
+        var width = tableData[0].length+1;
+        for (var i in tableData) {
+            tableData[i] = create_line(tableData[i],i);
+            if (tableData[i].length != width) {
+                alert('needs a message 2');
+                return;
+            }
+        }
+        var table = createInputTable("#"+target.prop("id"),createHeaders((width-1)/2,tableData),tableData).children('thead');
+        if (self.model.startYear && self.model.interval) {
+          var headerRow = $('<tr><th class="white-border"></th></tr>');
+          for (var i = 0; i < (width-1)/2; i++) {
+              var header = self.model.startYear+self.model.interval*i;
+              headerRow.append($('<th class="header" colspan="2"></th>').html(header + "-" + (header+self.model.interval-1)));
+          }
+          table.prepend(headerRow);
+        }
+        if (data.title && self.model.description) table.prepend('<tr><th class="white-border"></th><th class="header" colspan="'+(width-1)+'">'+data.title+'<br/><span class="blue">'+self.model.description+'</span></th></tr>');
+        target.children(".paste-here").remove();
+    };
 
     /* ---------- Saves the file contents to the model ---------- */
     function updateModel(id, contents) {
         self.model[id] = contents;
 
+        self.cfg.description.val(self.model[id].description);
         self.cfg.startAge.val(self.model[id].startAge);
         self.cfg.startYear.val(self.model[id].startYear);
         self.cfg.interval.val(self.model[id].interval);
@@ -368,40 +476,6 @@ var crosstalk = (function ($, ReadFile) {
         syncModel();
     }
 
-    function updateTable(id, contents) {
-
-        $('#table1').html('');
-
-        var table = document.createElement('table');
-
-        headers = self.model.inputfile1.table[0];
-
-        table.setAttribute('id', id);
-        table.setAttribute('class', 'table display compact');
-        table.setAttribute('width', '100%');
-
-        $('#table1').append(table);
-
-        var headers = [];
-
-        for (var i = 0; i < self.model.inputfile1.table[0].length / 2; i++)
-            headers.push({
-                title: (i).toString()
-            })
-
-        $('#tableA').DataTable({
-            'destroy': true,
-            'columns': headers,
-            'data': self.model.inputfile1.table,
-            'bSort': false,
-            'bFilter': false,
-            'paging': false,
-            'responsive': true,
-            'dom': 't'
-        });
-
-    }
-
     /* ---------- Saves DOM references in the configuration ---------- */
     function config(cfg) {
         self.cfg = cfg;
@@ -412,7 +486,48 @@ var crosstalk = (function ($, ReadFile) {
     function log() {
         console.log('Configuration', self.cfg);
         console.log('Model', self.model);
-        //        updateTable('tableA', self.model.inputfile1.table);
     }
 
+    function createHeaders(length,data) {
+      var ageHeader = "Age";
+      if (data !== undefined) ageHeader = '<span class = "ageGroups">' + data.length + " age groups </span>";
+      var headers = [{
+        title: ageHeader,
+        className: 'dt-center grey'
+      }];
+      for (var i = 0; i < length; i += 1) {
+        headers.push({
+          title: "Count",
+          className: 'dt-body-right'
+        });
+        headers.push({
+          title: "Population",
+          className: 'dt-body-right'
+        });
+      }
+      return headers;
+    };
+
+    function createInputTable(containerID, headers, data) {
+        var table = $(document.createElement('table'));
+        table.attr('class','table display compact');
+        table.attr('width','100%');
+        $(containerID).children(".dataTables_wrapper").children(".dataTable").DataTable().destroy();
+        $(containerID).children("table").remove();
+        $(containerID).prepend(table);
+        table.DataTable({
+            "destroy": true,
+            "data": data,
+            "columns": headers,
+            "bSort": false,
+            "bFilter": false,
+            "paging": false,
+            "responsive": true,
+            "dom": 't',
+            "scrollX": false
+        });
+        $(containerID).find('#inputTable_wrapper').addClass('table-responsive');
+        $(containerID).find('.table').addClass('input-table');
+        return table;
+    };
 })($, readfile);
