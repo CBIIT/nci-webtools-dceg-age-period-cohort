1117abd3ec71e538e9819bc24e68aeb35068a60a
park-brian <re.public@outlook.com>
2016-09-23 17:00:56 -0400

    updated python wrapper - using traceback module

--- crosstalk/crosstalk.py
+++ crosstalk/crosstalk.py
@@ -1,39 +1,19 @@
-from flask import Flask, request, jsonify
-from rpy2.robjects import r as wrapper
-import json
-import os
-import sys
+from flask import Flask, request
+from rpy2.robjects import r
+import traceback
 
 app = Flask(__name__)
+r.source('crosstalkWrapper.R')
 
-wrapper['source']('CrossTalkWrapper.R')
-def buildFailure(data):
-  response = jsonify(data=data, complete=False)
-  response.mimetype = 'application/json'
-  response.status_code = 400
-  return response
-
-def buildSuccess(data):
-  response = jsonify(data=data, complete=True)
-  response.mimetype = 'application/json'
-  response.status_code = 200
-  return response
-
-# Specify either calculate, fitModel, or generateExcel
-@app.route('/crosstalkRest/<action>', methods = ['POST'])
-def process(action):
+@app.route('/calculate/', methods = ['POST'])
+def calculate():
     try:
-      print(request.get_data())
-      
-      if action in ['calculate', 'fitModel', 'generateExcel', 'process']:
-        return buildSuccess(json.loads(wrapper[action](request.get_data())[0]))
+        return r.calculate(request.stream.read())[0]
 
     except Exception as e:
-      exc_type, exc_obj, exc_tb = sys.exc_info()
-      fname = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]
-      print("EXCEPTION------------------------------", exc_type, fname, exc_tb.tb_lineno)
-      return buildFailure(str(e))
-
+        print('------------EXCEPTION------------')
+        traceback.print_exc(1)
+        return str(e), 400
 
 @app.after_request
 def after_request(response):
@@ -42,11 +22,6 @@ def after_request(response):
     response.headers.add('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE')
     return response
 
-import argparse
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser()
-    # Default port is production value; prod, stage, dev = 8140, sandbox = 9140
-    parser.add_argument('-p', '--port', dest = 'port_num', type = int, default = '9140', help = 'Sets the Port')
-    parser.add_argument('-d', '--debug', action = 'store_true')
-    args = parser.parse_args()
-    app.run(host = '0.0.0.0', port = args.port_num, debug = args.debug, use_evalex = False)
+    app.run('0.0.0.0', port = 10000, debug = True)
+
