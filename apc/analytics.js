3397805800af6459680d9a517b8a6a8a6efcdebe
Brian Park <park-brian@users.noreply.github.com>
2018-11-05 15:18:46 -0500

    NCIATWP-816: add analytics snippet

---
+++ apc/analytics.js
@@ -0,0 +1,10 @@
+(function() {
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+    gtag('config', 'UA-62346354-2');
+
+    window.addEventListener('load', function() {
+        _satellite.pageBottom()
+    });
+});
