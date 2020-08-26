d0e43fe9438c5c50274d7a4287646249202e6865
Brian Park <park-brian@users.noreply.github.com>
2020-08-26 00:49:12 -0400

    serve index file when developing locally

--- apc/apc.py
+++ apc/apc.py
@@ -1,4 +1,4 @@
-from flask import Flask, request
+from flask import Flask, request, send_file
 from rpy2.robjects import r
 
 app = Flask(__name__, static_folder='', static_url_path='')
@@ -20,4 +20,7 @@ def error_handler(e):
     return str(e), 400
 
 if __name__ == '__main__':
+    @app.route('/')
+    def index():
+        return send_file('index.html')
     app.run('0.0.0.0', port = 10000, debug = True, use_reloader = True)
