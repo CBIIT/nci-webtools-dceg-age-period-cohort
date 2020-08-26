3ac4471cde2c9f7b02a888b0597c68ebd45b70e7
Brian Park <park-brian@users.noreply.github.com>
2020-08-25 20:56:52 -0400

    remove CORS headers

--- apc/apc.py
+++ apc/apc.py
@@ -19,12 +19,5 @@ def error_handler(e):
     app.logger.error(str(e))
     return str(e), 400
 
-@app.after_request
-def after_request(response):
-    response.headers.add('Access-Control-Allow-Origin', '*')
-    response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization')
-    response.headers.add('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE')
-    return response
-
 if __name__ == '__main__':
     app.run('0.0.0.0', port = 10000, debug = True, use_reloader = True)
